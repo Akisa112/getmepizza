b3fb1bb3b4292485db71425f900e409fcbf3a179
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2022-10-09 21:22:37 +0200

    finished homepage
    homepage design done
    starting the routing
    moved navbar to its own component

---
+++ pages/components/navbar.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import toast from 'react-hot-toast';
+
+export default function NavBar() {
+    const user = 'test';
+    const username = '';
+
+
+    return (
+        <main className=' bg-slate-50 pt-10 font-CircularMedium '>
+            <section className='px-3 py-3  md:px-20 md:bg-white lg:max-w-4xl lg:mx-auto rounded-full '>
+            <nav className='flex justify-between   '>
+                <Link href={'/'}><h1 className='text-2xl md:pl-4'>getme.🍕</h1></Link>
+                <ul className='flex items-center md:mr-4'>
+
+                {username && (
+                    <>
+                        <li className='mx-2  text-sm'>
+                            <Link href={`/admin`}>ADMIN</Link>
+                        </li>
+                        <li className='mx-2  text-sm'>
+                            <Link href={`/${username}`}><Image src={user?.photoURL}/></Link>
+                        </li>
+                    </>
+                )}
+
+                {!username && (
+                    <>
+                        <li className='mx-2  text-sm'>
+                            <Link href={"/faq"}>FAQ</Link>
+                        </li>
+                        <li className='mx-2  text-sm'>
+                            <Link href={"/explore"}>Explore</Link>
+                        </li>
+                        <li className='mx-2 bg-yellow-400 rounded-full text-sm px-4  py-2'>
+                            <Link href={"/enter"}>Sign up</Link>
+                        </li>
+                       
+                    </>
+                )}
+
+                
+                </ul>
+            </nav>
+            </section>
+        </main>
+    ) 
+}
