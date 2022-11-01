0badd87645cbf601e3dc0d0dbc5d58753322c5a7
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2022-11-01 08:48:51 +0200

    rafactor AdminPage

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -9,32 +9,27 @@ import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
 import toast from "react-hot-toast";
 import SideNav from "../components/sideNav";
 import { useDropzone } from "react-dropzone";
-import { getUserWithUsername } from "../lib/firebase";
+
 import { BiCamera } from "react-icons/bi";
 import { blobToBase64 } from "../lib/hooks";
 import { upLoadIPFS } from "../lib/moralis";
 
 export default function AdminPage({}) {
-  const { username } = useContext(UserContext);
+  return (
+    <main className='min-h-[calc(100vh-163px)] flex flex-col justify-between'>
+      <AuthCheck>
+        <AdminManager />
+      </AuthCheck>
+      <PoweredBy />
+    </main>
+  );
+}
 
-  const [userPhotoURL, setUserPhotoURL] = useState("");
+function AdminManager() {
+  const { username, userPhoto } = useContext(UserContext);
+
+  const [userPhotoURL, setUserPhotoURL] = useState(userPhoto);
   const [userEditedPhotoURL, setUserEditedPhotoURL] = useState("");
-
-  const fetchUser = async () => {
-    try {
-      const userDoc = await getUserWithUsername(username);
-      const user = userDoc.data();
-      setUserPhotoURL(user.photoURL);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  if (username) {
-    if (!userPhotoURL) {
-      fetchUser();
-    }
-  }
 
   let postRef = firestore.collection("users").doc(auth.currentUser.uid);
   let [user] = useDocumentDataOnce(postRef);
@@ -97,128 +92,124 @@ export default function AdminPage({}) {
 
     toast.success("Updated successfully!");
   };
-
   return (
-    <main className='min-h-[calc(100vh-163px)] flex flex-col justify-between'>
-      <AuthCheck>
-        <div className='left-[5%] top-24 hidden lg:block lg:absolute'>
-          <SideNav username={username} />
-        </div>
-        <form
-          className='mx-4 mt-6 md:w-[600px] md:mx-auto'
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className='mx-4 flex justify-between'>
-            <div className='w-16 mb-6 border-none  cursor-none'>
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                {isDragActive ? (
+    <>
+      <div className='left-[5%] top-24 hidden lg:block lg:absolute'>
+        <SideNav username={username} />
+      </div>
+      <form
+        className='mx-4 mt-6 md:w-[600px] md:mx-auto'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className='mx-4 flex justify-between'>
+          <div className='w-16 mb-6 border-none  cursor-none'>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <img
+                  className='w-[65px] h-[65px] rounded-full border-dashed border-gray-800 dark:border-slate-300 border-2 '
+                  src={userPhotoURL}
+                />
+              ) : (
+                <div className='relative'>
                   <img
-                    className='w-[65px] h-[65px] rounded-full border-dashed border-gray-800 dark:border-slate-300 border-2 '
+                    className='w-[65px] h-[65px] rounded-full border-solid border-gray-800 dark:border-slate-300 border-2 '
                     src={userPhotoURL}
                   />
-                ) : (
-                  <div className='relative'>
+                  {userEditedPhotoURL && (
                     <img
-                      className='w-[65px] h-[65px] rounded-full border-solid border-gray-800 dark:border-slate-300 border-2 '
-                      src={userPhotoURL}
+                      className='absolute top-0 w-[65px] h-[65px] bg-white rounded-full border-solid border-gray-800 dark:border-slate-300 border-2 '
+                      src={userEditedPhotoURL}
                     />
-                    {userEditedPhotoURL && (
-                      <img
-                        className='absolute top-0 w-[65px] h-[65px] bg-white rounded-full border-solid border-gray-800 dark:border-slate-300 border-2 '
-                        src={userEditedPhotoURL}
-                      />
-                    )}
-                    <div className='absolute top-5 left-5 p-1 bg-neutral-400 bg-opacity-80 rounded-full'>
-                      <BiCamera />
-                    </div>
+                  )}
+                  <div className='absolute top-5 left-5 p-1 bg-neutral-400 bg-opacity-80 rounded-full'>
+                    <BiCamera />
                   </div>
-                )}
-              </div>
-              <p className='mt-2 text-xs w-[200px]'>Drag n drop image.</p>
+                </div>
+              )}
             </div>
-
-            <div>
-              <h1 className='text-right mb-2 mt-2 font-CircularMedium text-2xl'>
-                {username}
-              </h1>
-              <small className='text-xs'>username cannot be changed*</small>
-            </div>
+            <p className='mt-2 text-xs w-[200px]'>Drag n drop image.</p>
           </div>
-          <h4 className='font-Montserrat mb-3 mx-2 text-left'>Display Name</h4>
-          <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800  mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
-            <input
-              className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
-              type='text'
-              placeholder={""}
-              ref={register}
-              {...register("displayName", { maxLength: 80 })}
-            />
-          </div>
-          <p className='text-red-500 mx-3 text-left mb-4'>
-            {errors.name && "Please enter a name"}
-          </p>
 
-          <h4 className='font-Montserrat mb-3 mx-2 text-left'>About</h4>
-          <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
-            <textarea
-              className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
-              rows={4}
-              placeholder={""}
-              {...register("about", {
-                minLength: 20,
-                maxLength: 128,
-              })}
-            />
+          <div>
+            <h1 className='text-right mb-2 mt-2 font-CircularMedium text-2xl'>
+              {username}
+            </h1>
+            <small className='text-xs'>username cannot be changed*</small>
           </div>
-          <p className='text-red-500 mx-3 text-left mb-4'>
-            {errors.about &&
-              "Must be atleast 20 characters and less than 128 characters"}
-          </p>
+        </div>
+        <h4 className='font-Montserrat mb-3 mx-2 text-left'>Display Name</h4>
+        <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800  mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
+          <input
+            className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
+            type='text'
+            placeholder={""}
+            ref={register}
+            {...register("displayName", { maxLength: 80 })}
+          />
+        </div>
+        <p className='text-red-500 mx-3 text-left mb-4'>
+          {errors.name && "Please enter a name"}
+        </p>
 
-          <h4 className='font-Montserrat mb-3 mx-2 text-left '>
-            Website or social link
-          </h4>
-          <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
-            <input
-              className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
-              type='url'
-              placeholder={""}
-              {...register("website", { maxLength: 80 })}
-            />
-          </div>
-          <p className='text-red-500 mx-3 text-left mb-4'>
-            {errors.website && "Please enter a valid link"}
-          </p>
+        <h4 className='font-Montserrat mb-3 mx-2 text-left'>About</h4>
+        <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
+          <textarea
+            className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
+            rows={4}
+            placeholder={""}
+            {...register("about", {
+              minLength: 20,
+              maxLength: 128,
+            })}
+          />
+        </div>
+        <p className='text-red-500 mx-3 text-left mb-4'>
+          {errors.about &&
+            "Must be atleast 20 characters and less than 128 characters"}
+        </p>
 
-          <h4 className='font-Montserrat mb-3 mx-2 text-left'>
-            EVM Address (ETH, MATIC, BSC, ETC)
-          </h4>
-          <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
-            <input
-              className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
-              type='text'
-              placeholder={""}
-              ref={register}
-              {...register("ethAddress", {
-                maxLength: 64,
-                pattern: /^0x[a-fA-F0-9]{40}$/g,
-              })}
-            />
-          </div>
-          <p className='text-red-500 mx-3 text-left mb-4'>
-            {errors.ethAddress && "Please enter a valid address"}
-          </p>
+        <h4 className='font-Montserrat mb-3 mx-2 text-left '>
+          Website or social link
+        </h4>
+        <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
+          <input
+            className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
+            type='url'
+            placeholder={""}
+            {...register("website", { maxLength: 80 })}
+          />
+        </div>
+        <p className='text-red-500 mx-3 text-left mb-4'>
+          {errors.website && "Please enter a valid link"}
+        </p>
 
-          <button
-            type='submit'
-            className=' bg-yellow-300 rounded-full mt-6 py-3 h-[50px] min-w-full text-center font-CircularMedium md:max-w-xs md:mx-auto hover:scale-105 transition-all dark:text-black'
-          >
-            Save Changes
-          </button>
-        </form>
-      </AuthCheck>
-      <PoweredBy />
-    </main>
+        <h4 className='font-Montserrat mb-3 mx-2 text-left'>
+          EVM Address (ETH, MATIC, BSC, ETC)
+        </h4>
+        <div className='mx-2 text-center py-2  rounded-lg bg-white dark:bg-zinc-800 mb-4 text-lg border-2 border-black dark:border-slate-300  lg:mx-auto'>
+          <input
+            className='w-full px-4 border-none focus:ring-0 dark:bg-zinc-800'
+            type='text'
+            placeholder={""}
+            ref={register}
+            {...register("ethAddress", {
+              maxLength: 64,
+              pattern: /^0x[a-fA-F0-9]{40}$/g,
+            })}
+          />
+        </div>
+        <p className='text-red-500 mx-3 text-left mb-4'>
+          {errors.ethAddress && "Please enter a valid address"}
+        </p>
+
+        <button
+          type='submit'
+          className=' bg-yellow-300 rounded-full mt-6 py-3 h-[50px] min-w-full text-center font-CircularMedium md:max-w-xs md:mx-auto hover:scale-105 transition-all dark:text-black'
+        >
+          Save Changes
+        </button>
+      </form>
+    </>
   );
 }
